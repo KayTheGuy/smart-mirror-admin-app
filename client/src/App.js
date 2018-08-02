30327c12e96f3d9f35a13e9f90cacc66d3909fe4
Kayhan <kdehghan@sfu.ca>
2018-08-02 16:14:31 -0700

    Add initial connection between React and Node

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,31 @@
-// Responsible for Express and listening to requests
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    response: '',
+    loading: true
+  };
+
+  componentDidMount() {
+    this.callApi();
+  }
+    
+  callApi = async () => {
+    fetch('/api/test')
+      .then(response => { return response.json(); })
+      .then(myJson => { this.setState({ response: myJson.a}); })
+      .catch(err => { console.log(err); });
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">Some header here...</header>
+          <p className="App-intro">{this.state.response}</p>
+        </div>
+      );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-// Responsible for Express and listening to requests
+const express = require('express');
+const app = express();
+
+app.get('/api/test', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ a: 1 });
+});
+  
+app.listen(5000);
