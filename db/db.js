51471b3e2518cd100cbc777b5ac5400f356f864c
Kai Sackville-Hii <sackvillehiik@gmail.com>
2018-08-02 12:39:59 -0700

    db functions updated

--- db/db.js
+++ db/db.js
@@ -18,6 +18,75 @@ var readAll = function (dbName, collectionName) {
    });
 };
 
+var writeObject = function (dbName, collectionName, data) {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+            const col = client.db(dbName).collection(collectionName);
+            col.insert(data, (err, doc) => {
+                if (err) {
+                    console.log(`Failed to insert items for db: ${dbName}, collection: ${collectionName}\n${err}`);
+                    reject();
+                }
+                else {
+                    client.close();
+                    resolve(200);
+                }
+            });
+        });
+    });
+};
+
+var updateObjectKey = function (dbName, collectionName, id, key, value) {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+            const col = client.db(dbName).collection(collectionName);
+            const myquery = { _id: id };
+
+            let tempObj = {};
+            tempObj[key] = value;
+
+            let newvalues = { $set: tempObj };
+
+            col.updateOne(myquery, newvalues, (err, doc) => {
+                if (err) {
+                    console.log(`Failed to insert items for db: ${dbName}, collection: ${collectionName}, id: ${id}\n${err}`);
+                    reject();
+                }
+                else {
+                    client.close();
+                    resolve(200);
+                }
+            });
+        });
+    });
+};
+
+var deleteObject = function (dbName, collectionName, id) {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+            const col = client.db(dbName).collection(collectionName);
+
+            let myquery = { _id: id };
+
+            col.deleteOne(myquery, (err, doc) => {
+                if (err) {
+                    console.log(`Failed to delete items for db: ${dbName}, collection: ${collectionName}, id: ${id}\n${err}`);
+                    reject();
+                }
+                else {
+                    client.close();
+                    resolve(200);
+                }
+            });
+        });
+    });
+};
+
+deleteObject("db1", "col1", "5b6356c25dc3170474db25ff");
+
 module.exports = {
     readAll,
+    writeObject,
+    updateObjectKey,
+    deleteObject
 };
--- db/test/db.test.js
+++ db/test/db.test.js
@@ -1,12 +1,58 @@
 const assert = require("assert");
 const db = require("../db.js");
 
-db.readAll("db1", "col1")
-.then( (data) => { assert.notEqual(data, undefined, "readAll failed for empty collection!") } )
-.catch( (err) => { console.log(err); } );
-db.readAll("db1", "col1")
-.then( (data) => { assert.notDeepEqual(data, [1], "readAll failed for empty collection!") } )
-.catch( (err) => { console.log(err); } );
-db.readAll("db1", "col1")
-.then( (data) => { assert.deepEqual(data, [], "readAll failed for empty collection!") } )
-.catch( (err) => { console.log(err); } );
+
+function writeTest() {
+    db.writeObject("db1", "col1", {"_id":"5b6356c25dc3170474db25fd","db_test":1})
+        .then( (data) => {
+            assert.equal(data, 200, "writeObject failed for one object");
+            db.writeObject("db1", "col1", [{"_id":"5b6356c25dc3170474db25fe","db_test":2}, {"_id":"5b6356c25dc3170474db25ff","db_test":3}])
+                .then( (data) => { assert.equal(data, 200, "writeObject failed for multiple objects ")})
+                .catch( (err) => { console.log(err) } );
+        })
+        .catch(  (err) => { console.log(err) } );
+}
+
+function readTest() {
+    db.readAll("db1", "col1")
+        .then( (data) => { assert.notEqual(data, undefined, "readAll failed for empty collection!") } )
+        .catch( (err) => { console.log(err); } );
+    db.readAll("db1", "col1")
+        .then( (data) => { assert.notDeepEqual(data, [1], "readAll failed for empty collection!") } )
+        .catch( (err) => { console.log(err); } );
+    db.readAll("db1", "col1")
+        .then( (data) => {
+            assert.deepEqual(data, [
+                    {"_id":"5b6356c25dc3170474db25fd","db_test":1},
+                    {"_id":"5b6356c25dc3170474db25fe","db_test":2},
+                    {"_id":"5b6356c25dc3170474db25ff","db_test":3}
+                ],
+                "readAll failed"
+            )} )
+        .catch( (err) => { console.log(err); } );
+}
+
+function updateTest() {
+    db.updateObjectKey("db1", "col1", "5b6356c25dc3170474db25fd", "db_test", 1000)
+        .then( (data) => {
+            assert.equal(data, 200, "updateObjectKey failed");
+            db.updateObjectKey("db1", "col1", "5b6356c25dc3170474db25fd", "db_test", 1)
+                .then( (data) => { assert.equal(data, 200, "updateObjectKey failed")})
+                .catch( (err) => { console.log(err); } );
+        })
+        .catch( (err) => { console.log(err); } );
+
+}
+
+function deleteTest() {
+    db.deleteObject("db1", "col1", "5b6356c25dc3170474db25fd")
+        .then( (data) => {assert.equal(data, 200, "deleteTest failed");})
+        .catch( (err) => { console.log(err); } );
+}
+
+setTimeout(() => writeTest(), 0);
+setTimeout(() => readTest(), 2000);
+setTimeout(() => updateTest(), 4000);
+setTimeout(() => readTest(), 6000);
+setTimeout(() => deleteTest(), 8000);
+
