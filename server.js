372fd2ad853403630469e4da87e44bd7420a9007
Kayhan <kdehghan@sfu.ca>
2018-08-03 14:55:51 -0700

    Add test post method

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const PORT = 5000;
 
-app.get('/api/test', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/forms/type', (req, res) => {
+    if (type === "event") {
+
+    } else if ((type === "news")) {
+
+    }
     res.setHeader('Content-Type', 'application/json');
-    res.json({ a: 1 });
+    res.json({
+        a: 1
+    });
 });
-  
-app.listen(5000);
+
+app.post('/events/', function (req, res) {
+    console.log(req.body);
+    res.status(200).send();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started listening on port ${PORT} ...`);
+});
