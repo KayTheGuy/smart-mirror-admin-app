da7404361fcf580b2009b096a616afc41b0cb961
Kayhan <kdehghan@sfu.ca>
2018-08-16 13:50:13 -0700

    Add initial image uploader
    Co-authored-by: Wongi <gospel.teka@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const db = require('./db/db');
 const formType = require('./model/form-type').forms;
 const dbName = require('./model/form-type').dbName;
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '150mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '150mb', extended: true}))
+
+var upload = multer({ dest: 'uploads/' })
 
 app.get('/forms/type', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
@@ -16,15 +19,17 @@ app.get('/forms/type', (req, res) => {
 		res.status(200).json(data);
 	})
 	.catch((err) => {
+		console.log(err);
 		res.status(500).json(null);
 	});
 });
 
-app.post('/form/:type', function (req, res) {
-	if (req.body) {
-		db.writeObject(dbName, req.params.type, req.body);
-	}
-	res.status(200).json({'a': 12});
+app.post('/form/:type', upload.array('files', 12), function (req, res) {
+	console.log(req.body);
+	console.log(req.files);
+	// db.writeObject(dbName, req.params.type, req.body)
+	// .then(status => res.status(status).send())
+	// .catch(() => console.log(`Failed to post form of type ${req.param.type}`))
 });
 
 app.listen(PORT, () => {
